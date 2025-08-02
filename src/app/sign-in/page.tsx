1fad0842b06ab6147324310ad10d40d6d6f1d9ba
Harry <sahilbhaisharma1212@gmail.com>
2025-08-02 23:39:56 +0530

    routing logic change in sign in page due to delays

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,51 +1,64 @@
 "use client"
-import React from 'react';
+import React, { useState } from 'react';
 import { FaGoogle } from "react-icons/fa";
 import { auth } from '@/config/firebaseConfig';
 import {
-  GoogleAuthProvider,
-  signInWithPopup,
-  signOut,
+    GoogleAuthProvider,
+    signInWithPopup,
+    signOut,
 } from "firebase/auth";
-import { useRouter } from "next/navigation";
-import toast , {Toaster} from "react-hot-toast"
+import toast, { Toaster } from "react-hot-toast"
 import { MdOutlinePhoneIphone } from "react-icons/md";
 import axios from "axios"
+import Link from 'next/link';
 
 export default function Page() {
-
-    const router = useRouter();
+    const [signinSuccessfull, setSignInSuccessfull] = useState(false)
 
     const handleGoogleSignIn = async () => {
 
-    try {
-      const provider = new GoogleAuthProvider();
-      const result = await signInWithPopup(auth, provider);
+        try {
+            const provider = new GoogleAuthProvider();
+            const result = await signInWithPopup(auth, provider);
 
-      const email = result.user.email;
+            const email = result.user.email;
 
-      const { data } = await axios.post("/api/verify-user", { email });
+            const { data } = await axios.post("/api/verify-user", { email });
 
-      if (data.allowed) {
-        toast.success("Access granted");
-        router.push("/admin");
-      } else {
-        await signOut(auth);
-        toast.error("Access denied: not whitelisted");
-      }
-    } catch (err) {
-      console.error(err);
-      toast.error("Google Sign-in failed");
-    }
-  };
+            if (data.allowed) {
+                toast.success("Access granted");
+                setSignInSuccessfull(true)
+            } else {
+                await signOut(auth);
+                toast.error("Access denied: not whitelisted");
+            }
+        } catch (err) {
+            console.error(err);
+            toast.error("Google Sign-in failed");
+        }
+    };
 
     return (
-        <div className=" min-h-150 flex items-center justify-center px-4">
-            <div className="bg-white rounded-2xl shadow-xl w-full max-w-md p-8 space-y-6">
-                <h1 className="text-3xl font-semibold text-gray-900 text-center">Sign In</h1>
-                <p className='text-center'>Welcome to E Malkhana</p>
-                
-                   
+        signinSuccessfull ? (
+            <div className="h-[32rem] flex justify-center items-center p-5 bg-green-50">
+                <div className="bg-white rounded-2xl shadow-lg p-8 max-w-md w-full text-center space-y-6">
+                    <h2 className="text-2xl font-semibold text-green-600">Signed in Successfully</h2>
+                    <p className="text-gray-700">You can now access the admin panel.</p>
+
+                    <Link href="/admin">
+                        <button className="bg-green-600 hover:bg-green-700 text-white font-medium px-6 py-2 rounded-lg transition duration-200">
+                            Go to Admin Panel
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        ) : (
+            <div className=" min-h-150 flex items-center justify-center px-4">
+                <div className="bg-white rounded-2xl shadow-xl w-full max-w-md p-8 space-y-6">
+                    <h1 className="text-3xl font-semibold text-gray-900 text-center">Sign In</h1 >
+                    <p className='text-center'>Welcome to E Malkhana</p>
+
+
                     {/* Google */}
                     <div className="flex justify-between items-center pt-4">
                         <button
@@ -69,7 +82,7 @@ export default function Page() {
                     <div className="flex justify-between items-center pt-4">
                         <button
                             className="bg-gray-900 hover:bg-gray-700 text-white font-medium px-5 py-2 rounded-md transition-all duration-200 w-full flex justify-center items-center gap-2"
-                            onClick={(e)=>{
+                            onClick={(e) => {
                                 e.preventDefault()
                                 toast("Feature not added yet")
                             }}
@@ -88,9 +101,11 @@ export default function Page() {
 
                     </div>
 
-            
-            </div>
-            <Toaster/>
-        </div>
+
+                </div >
+                <Toaster />
+            </div >
+        )
+
     );
 }
