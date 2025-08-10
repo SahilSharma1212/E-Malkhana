7edf0fcfaeb256d6b7e2ace4eeb9999cd14229fe
Harry <sahilbhaisharma1212@gmail.com>
2025-08-10 16:49:46 +0530

    navbar admin button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
 import axios from 'axios';
-import { LogOut, QrCode, Search } from 'lucide-react';
+import { LogOut, QrCode, Search, User } from 'lucide-react';
 
 export default function Navbar() {
   const [hasToken, setHasToken] = useState(false);
@@ -51,15 +51,17 @@ export default function Navbar() {
 
   return (
     <nav className="w-full bg-blue-100 text-white shadow-md py-2 px-1.5">
-      <div className="w-full mx-auto px-4 py-3 flex items-center justify-between bg-blue-500 rounded-sm pr-10 max-sm:pr-4">
-        <Link href={'/admin'} className="flex justify-center gap-4 items-center">
+      <div className="w-full mx-auto px-4 py-3 flex items-center justify-between bg-blue-500 rounded-sm pr-10 max-sm:pr-4 max-[350px]:pl-1">
+
+        {/* image and malkhana name */}
+        <Link href={'/admin'} className="flex justify-center gap-4 items-center max-sm:gap-2">
           <div className="flex items-center justify-center">
             <Image
               src="/CG_POLICE_LOGO.png"
               alt="Logo"
               width={20}
               height={20}
-              className="object-contain"
+              className="object-contain max-[350px]:hidden"
             />
           </div>
 
@@ -73,23 +75,23 @@ export default function Navbar() {
         </Link>
 
         {/* Navigation Links */}
-        <div className="space-x-4 text-sm md:text-base max-md:text-sm flex items-center justify-center">
+        <div className="space-x-4 text-sm md:text-base max-md:text-sm flex items-center justify-center max-sm:space-x-1 max-sm:scale-95">
           {hasToken && (
             <>
 
               {/* scanner */}
               <button
-                className="hover:underline text-sm md:text-base max-md:text-sm px-2 py-1 rounded-md  border-white text-white border hover:bg-blue-400 max-sm:scale-90 sm:border"
+                className="hover:underline text-sm md:text-base max-md:text-sm px-2 py-1 rounded-md  border-blue-300 text-white border hover:bg-blue-400 max-sm:scale-90 sm:border"
               >
                 <Link href={"/scanner"} className="">
                   <QrCode strokeWidth={1} />
                 </Link>
               </button>
 
-              {/* search */}
+              {/* signout small */}
               <button
                 onClick={handleSignOut}
-                className="hover:underline text-sm md:text-base max-md:text-sm px-2 py-1 rounded-md  border-white text-white border sm:hidden hover:bg-blue-400 max-sm:scale-90"
+                className="hover:underline text-sm md:text-base max-md:text-sm px-2 py-1 rounded-md  border-blue-300 text-white border sm:hidden hover:bg-blue-400 max-sm:scale-90"
               >
                 <LogOut strokeWidth={1} />
               </button>
@@ -103,14 +105,26 @@ export default function Navbar() {
               </button>
 
 
+
+
+
             </>
           )}
           <Link href="/search-property" className="text-sm md:text-base max-md:text-sm px-3 py-1 rounded-md max-sm:hidden border-blue-300 text-white border font-medium  hover:bg-blue-400">
             Search
           </Link>
-          <Link href="/search-property" className="px-2 py-1 hover:bg-blue-400 rounded-sm transition-all sm:hidden border border-white max-sm:scale-90">
+          <Link href="/search-property" className="px-2 py-1 hover:bg-blue-400 rounded-sm transition-all sm:hidden border border-blue-300 max-sm:scale-90">
             <Search strokeWidth={1} />
           </Link>
+
+          {/* admin */}
+          <button
+            className="hover:underline text-sm md:text-base max-md:text-sm px-2 py-1 rounded-md  border-blue-300 text-white border hover:bg-blue-400 max-sm:scale-90 sm:border"
+          >
+            <Link href={"/admin"} className="">
+              <User strokeWidth={1} />
+            </Link>
+          </button>
 
         </div>
       </div>
